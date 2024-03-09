3bd483a80a7eede1baae19117476a09278d7d136
Gildas <gildas.lormeau@gmail.com>
2024-03-09 21:39:26 +0100

    destroy node_modules folder before compiling

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -rf /node_modules
+
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-aarch64-apple-darwin --target=aarch64-apple-darwin ./single-file
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-x86_64-apple-darwin --target=x86_64-apple-darwin ./single-file
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-x86_64-linux --target=x86_64-unknown-linux-gnu ./single-file
