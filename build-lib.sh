819dde58a427df126e5d981ec6dfe7dde8d2b4ab
Gildas <gildas.lormeau@gmail.com>
2024-03-11 02:17:31 +0100

    remove dev dependencies

--- build-lib.sh
+++ build-lib.sh
@@ -1,9 +1,81 @@
 #!/bin/sh
 
-npm install
-npm update
+deno vendor "npm:single-file-core"
 
-npx rollup -c rollup.config.js
-deno run --allow-read scripts/build-resources-script.js > lib/single-file-bundle.js
+echo "
+import { build } from 'npm:esbuild';
 
-find lib -type f ! -name 'single-file-bundle.js' -delete
+await build({
+  entryPoints: [
+    'node_modules/single-file-core/single-file.js'
+  ],
+  bundle: true,
+  globalName: 'singlefile',
+  outdir: 'lib/',
+  platform: 'browser',
+  sourcemap: false,
+  minify: true,
+  format: 'iife',
+  plugins: [],
+});
+
+await build({
+  entryPoints: [
+    'node_modules/single-file-core/single-file-bootstrap.js'
+  ],
+  bundle: true,
+  globalName: 'singlefileBootstrap',
+  outdir: 'lib/',
+  platform: 'browser',
+  sourcemap: false,
+  minify: true,
+  format: 'iife',
+  plugins: [],
+});
+
+await build({
+  entryPoints: [
+    'node_modules/single-file-core/single-file-hooks-frames.js'
+  ],
+  bundle: true,
+  outdir: 'lib/',
+  platform: 'browser',
+  sourcemap: false,
+  minify: true,
+  format: 'iife',
+  plugins: [],
+});
+
+await build({
+  entryPoints: [
+    'node_modules/single-file-core/vendor/zip/zip.min.js'
+  ],
+  bundle: true,
+  globalName: 'zip',
+  outdir: 'lib/',
+  platform: 'browser',
+  sourcemap: false,
+  minify: true,
+  format: 'iife',
+  plugins: [],
+});
+
+const SCRIPTS = [
+	'lib/single-file.js',
+	'lib/single-file-bootstrap.js',
+	'lib/single-file-hooks-frames.js',
+	'lib/zip.min.js'
+];
+
+let script = '';
+const scripts = SCRIPTS.map(script => Deno.readTextFile(new URL(script, import.meta.url)));
+const sources = await Promise.all(scripts);
+script += 'const script = ' + JSON.stringify(sources.join(';')) + ';';
+const zipScript = await Deno.readTextFile(new URL('lib/zip.min.js', import.meta.url));
+script += 'const zipScript = ' + JSON.stringify(zipScript) + ';';
+script += 'export { script, zipScript };';
+await Deno.writeTextFile(new URL('lib/single-file-bundle.js', import.meta.url), script)
+await Promise.all(SCRIPTS.map(script => Deno.remove(script)));
+" |  deno run --allow-read --allow-write --allow-net --allow-run --allow-env -
+
+rm -rf node_modules
--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf /node_modules
+rm -rf node_modules
 
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-aarch64-apple-darwin --target=aarch64-apple-darwin ./single-file
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-x86_64-apple-darwin --target=x86_64-apple-darwin ./single-file
