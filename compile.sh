1615cbd036f59faca016b0209ed04da84920e603
Gildas <gildas.lormeau@gmail.com>
2024-08-28 22:50:46 +0200

    sign binaries on macOS

--- compile.sh
+++ compile.sh
@@ -10,4 +10,8 @@ deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ex
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-aarch64-linux --target=aarch64-unknown-linux-gnu ./single-file
 deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file.exe --target=x86_64-pc-windows-msvc ./single-file
 
+dev_id=$(security find-identity -p codesigning -v | grep "Apple Development" | awk '{print $2}')
+codesign -f -s $dev_id ./dist/single-file-aarch64-apple-darwin
+codesign -f -s $dev_id ./dist/single-file-x86_64-apple-darwin
+
 mv ./package.json.copy ./package.json
