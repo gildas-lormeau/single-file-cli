dfea9377058e60ec0012e6a2e31907838fea8ed2
Gildas <gildas.lormeau@gmail.com>
2024-09-16 21:57:43 +0200

    fix description

--- options.js
+++ options.js
@@ -133,7 +133,7 @@ const OPTIONS_INFO = {
 	"prevent-appended-data": { description: "Prevent appending data after the compressed data when creating self-extracting HTML files", type: "boolean" },
 	"embed-screenshot": { description: "Embed a screenshot of the page as a PNG file in the compressed file (self-extracting HTML or ZIP file). When enabled, the resulting file can be read as a ZIP file or a PNG image.", type: "boolean" },
 	"embedded-image": { description: "Path to a PNG image to embed in the compressed file.", type: "string" },
-	"embed-pdf": { description: "Embed a PDF file in the self-extracting HTML file. When enabled, the resulting file can be read as a HTML, a ZIP file or a PDF file.", type: "boolean" },
+	"embed-pdf": { description: "Embed a PDF file in the ZIP or self-extracting file. When enabled, the resulting file can be read as a ZIP file or a PDF file.", type: "boolean" },
 	"embed-pdf-options": { description: "Options passed to the CDP method `Page.printToPDF()` given as a JSON string (e.g. { \"pageRanges\": \"1-1\", \"paperWidth\": 11, \"paperHeight\": 8.5 })", type: "string" },
 	"output-directory": { description: "Path to where to save files, this path must exist.", type: "string" },
 	"version": { description: "Print the version number and exit.", type: "boolean" },
