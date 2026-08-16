eb10bc4b12fd530673c270a104307dada5f622e8
Gildas <gildas.lormeau@gmail.com>
2026-08-16 17:51:20 +0200

    compile executables from a clean staging directory

--- compile.sh
+++ compile.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-cp ./package.json ./package.json.copy
-jq 'del(.dependencies)' ./package.json.copy > ./package.json
-mv node_modules node_modules.copy
-mv deno.lock deno.lock.copy
+set -e
 
-deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-aarch64-apple-darwin --target=aarch64-apple-darwin ./single-file
-deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-x86_64-apple-darwin --target=x86_64-apple-darwin ./single-file
-deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-x86_64-linux --target=x86_64-unknown-linux-gnu ./single-file
-deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output=./dist/single-file-aarch64-linux --target=aarch64-unknown-linux-gnu ./single-file
-deno compile --allow-all --ext=js --output=./dist/single-file.exe --target=x86_64-pc-windows-msvc --icon=./resources/single-file.ico ./single-file
+dist_dir=$(pwd)/dist
+stage_dir=$(mktemp -d)
+trap 'rm -rf "$stage_dir"' EXIT
+
+cp ./single-file ./single-file-launcher.js ./single-file-cli-api.js ./options.js ./deno.json "$stage_dir"
+cp -R ./lib "$stage_dir/lib"
+cp -R ./resources "$stage_dir/resources"
+jq 'del(.dependencies, .devDependencies)' ./package.json > "$stage_dir/package.json"
+mkdir -p "$dist_dir"
+cd "$stage_dir"
+
+deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output="$dist_dir/single-file-aarch64-apple-darwin" --target=aarch64-apple-darwin ./single-file
+deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output="$dist_dir/single-file-x86_64-apple-darwin" --target=x86_64-apple-darwin ./single-file
+deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output="$dist_dir/single-file-x86_64-linux" --target=x86_64-unknown-linux-gnu ./single-file
+deno compile --allow-read --allow-write --allow-net --allow-env --allow-run --ext=js --output="$dist_dir/single-file-aarch64-linux" --target=aarch64-unknown-linux-gnu ./single-file
+deno compile --allow-all --ext=js --output="$dist_dir/single-file.exe" --target=x86_64-pc-windows-msvc --icon=./resources/single-file.ico ./single-file
+
+cd - > /dev/null
 
 dev_id=$(security find-identity -p codesigning -v | grep "Apple Development" | awk '{print $2}')
 codesign -f -s $dev_id ./dist/single-file-aarch64-apple-darwin
 codesign -f -s $dev_id ./dist/single-file-x86_64-apple-darwin
-
-mv ./package.json.copy ./package.json
-mv node_modules.copy node_modules
-mv deno.lock.copy deno.lock
