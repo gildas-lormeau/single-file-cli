fccd3f74f572c1c7efa04bc6c716ebe5de042350
Gildas <gildas.lormeau@gmail.com>
2026-09-21 23:06:34 +0200

    update single-file-core

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CORE_PACKAGE="npm:single-file-core@1.6.7"
+CORE_PACKAGE="npm:single-file-core@1.6.8"
 ESBUILD_PACKAGE="npm:esbuild@0.27.7"
 WEB_STREAMS_PACKAGE="npm:web-streams-polyfill@4.3.0"
 
